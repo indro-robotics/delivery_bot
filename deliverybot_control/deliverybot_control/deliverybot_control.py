c805983d5b97e6b791607b2324feda7430c35f0a
liam-dwyer <liamd@indrorobotics.com>
2023-04-18 14:15:14 -0400

    Code cleanup

--- deliverybot_control/deliverybot_control/deliverybot_control.py
+++ deliverybot_control/deliverybot_control/deliverybot_control.py
@@ -20,42 +20,53 @@ vel_msg = Twist()
 class DeliverybotControlNode(Node):
     def __init__(self):
         super().__init__('deliverybot_control')
+
+        # Declaring variables
         timer_period = 0.02
         self.base_length = 0.75
         self.base_width = 0.30
-        self.wheel_radius = 0.0125
-        self.steer_offset = 0.15  # CHECK VALUE
-        self.steering_track = self.base_width - 2*self.steer_offset
-
         self.vel_msg_prev = 0.0
         self.steer_msg_prev = 0.0
-
         self.steer_pos = np.array([0, 0], float)
-        self.vel = np.array([0, 0, 0, 0], float) #RR, RL, FR, FL
-
+        self.vel = np.array([0, 0, 0, 0], float)  # RR, RL, FR, FL
         self.door_pos_prev = np.array([0], float)
-        self.steer_pos_prev =np.array([0, 0], float)
+        self.steer_pos_prev = np.array([0, 0], float)
 
+        # Creating Publishers
         self.pub_vel_ = self.create_publisher(
             Float64MultiArray, '/forward_velocity_controller/commands', 10)
         self.timer_ = self.create_timer(timer_period, self.timer_callback)
 
+        #Creating Subscribers
         self._jointStates_subscription = self.create_subscription(
             JointState,
             '/joint_states',
             self.jointStates_callback,
             10
         )
+
+        #Creating Actions
         self._action_door_client = ActionClient(
             self, FollowJointTrajectory, '/door_position_controller/follow_joint_trajectory')
         self._action_steer_client = ActionClient(
             self, FollowJointTrajectory, '/steering_trajectory_controller/follow_joint_trajectory')
 
-
     def theta_out(self, delta_ack):
+        """
+        Determines the ackermann steering angle of the outer wheel
+
+        :param delta_ack: goal steering angle of virtual center wheel
+        :return: ackermann steering angle of outer wheel theta_out
+        """
         return float(math.atan((self.base_length*math.atan(delta_ack))/(self.base_length + 0.5*self.base_width*math.atan(delta_ack))))
 
     def theta_in(self, delta_ack):
+        """
+        Determines the ackermann steering angle of the inner wheel
+
+        :param delta_ack: goal steering angle of virtual center wheel
+        :return: ackermann steering angle of inner wheel theta_out
+        """
         return float(math.atan((self.base_length*math.atan(delta_ack))/(self.base_length - 0.5*self.base_width*math.atan(delta_ack))))
 
     def timer_callback(self):
@@ -63,23 +74,23 @@ class DeliverybotControlNode(Node):
 
         sign = np.sign(vel_msg.linear.x)
         if vel_msg.angular.z > 0:
-            self.vel[0] = sign * np.abs(vel_msg.linear.x * \
-                (np.sin(vel_msg.angular.z) / np.sin(self.steer_pos[0])))
-            self.vel[1] = sign * np.abs(vel_msg.linear.x * \
-                (np.sin(vel_msg.angular.z) / np.sin(self.steer_pos[1])))
-            self.vel[2] = sign * np.abs(vel_msg.linear.x * \
-                (np.sin(vel_msg.angular.z) / np.sin(self.steer_pos[0])))
-            self.vel[3] = sign * np.abs(vel_msg.linear.x * \
-                (np.sin(vel_msg.angular.z) / np.sin(self.steer_pos[1])))
+            self.vel[0] = sign * np.abs(vel_msg.linear.x *
+                                        (np.sin(vel_msg.angular.z) / np.sin(self.steer_pos[0])))
+            self.vel[1] = sign * np.abs(vel_msg.linear.x *
+                                        (np.sin(vel_msg.angular.z) / np.sin(self.steer_pos[1])))
+            self.vel[2] = sign * np.abs(vel_msg.linear.x *
+                                        (np.sin(vel_msg.angular.z) / np.sin(self.steer_pos[0])))
+            self.vel[3] = sign * np.abs(vel_msg.linear.x *
+                                        (np.sin(vel_msg.angular.z) / np.sin(self.steer_pos[1])))
         if vel_msg.angular.z < 0:
-            self.vel[0] = sign * np.abs(vel_msg.linear.x * \
-                (np.sin(vel_msg.angular.z) / np.sin(self.steer_pos[0])))
-            self.vel[1] = sign * np.abs(vel_msg.linear.x * \
-                (np.sin(vel_msg.angular.z) / np.sin(self.steer_pos[1])))
-            self.vel[2] = sign * np.abs(vel_msg.linear.x * \
-                (np.sin(vel_msg.angular.z) / np.sin(self.steer_pos[0])))
-            self.vel[3] = sign * np.abs(vel_msg.linear.x * \
-                (np.sin(vel_msg.angular.z) / np.sin(self.steer_pos[1])))
+            self.vel[0] = sign * np.abs(vel_msg.linear.x *
+                                        (np.sin(vel_msg.angular.z) / np.sin(self.steer_pos[0])))
+            self.vel[1] = sign * np.abs(vel_msg.linear.x *
+                                        (np.sin(vel_msg.angular.z) / np.sin(self.steer_pos[1])))
+            self.vel[2] = sign * np.abs(vel_msg.linear.x *
+                                        (np.sin(vel_msg.angular.z) / np.sin(self.steer_pos[0])))
+            self.vel[3] = sign * np.abs(vel_msg.linear.x *
+                                        (np.sin(vel_msg.angular.z) / np.sin(self.steer_pos[1])))
         if vel_msg.angular.z == 0:
             self.vel[0] = vel_msg.linear.x
             self.vel[1] = vel_msg.linear.x
@@ -97,19 +108,26 @@ class DeliverybotControlNode(Node):
         steer_msg = vel_msg.angular.z
 
         self.door_pos_prev = [float(joint_positions[4])]
-        self.steer_pos_prev = [float(joint_positions[0]), float(joint_positions[1])]
+        self.steer_pos_prev = [
+            float(joint_positions[0]), float(joint_positions[1])]
         if np.abs(np.sum(self.steer_pos_prev)) < 0.004:
-                self.steer_pos_prev = [0.0, 0.0]
+            self.steer_pos_prev = [0.0, 0.0]
         if vel_msg.linear.z != self.vel_msg_prev:
             self.get_logger().info('Door Toggled.')
             self.send_door_goal()
 
-        if np.abs(steer_msg) > 0.610865: #35 degrees in radians (Steering limit)
+        if np.abs(steer_msg) > 0.610865:  # 35 degrees in radians (Steering limit)
             steer_msg = float(np.sign(steer_msg) * 0.610865)
         if steer_msg != self.steer_msg_prev:
-           self.send_steer_goal(steer_msg)
+            self.send_steer_goal(steer_msg)
 
     def send_door_goal(self):
+        '''
+        door_joint command controller to send joint_trajectory messages to control movement
+        :requires: joint_states topic and jointStates_callback - returns current door position information \n
+        :requires: *CURRENT* cmd_vel topic for control - TODO change to a service /door_control
+        :returns: nothing - calls on action server
+        '''
         global vel_msg
         self.vel_msg_prev = vel_msg.linear.z
         goal_msg = FollowJointTrajectory.Goal()
@@ -119,7 +137,6 @@ class DeliverybotControlNode(Node):
 
         if vel_msg.linear.z > 0:
             door_pos = [-1.5708]
-
         if vel_msg.linear.z < 0:
             door_pos = [0.0]
         if vel_msg.linear.z == 0:
@@ -131,8 +148,10 @@ class DeliverybotControlNode(Node):
         door_time = norm_coeff * 3
 
         point2 = JointTrajectoryPoint()
-        point2.time_from_start = Duration(seconds=door_time, nanoseconds=0).to_msg()
+        point2.time_from_start = Duration(
+            seconds=door_time, nanoseconds=0).to_msg()
         point2.positions = door_pos
+        
         if point1 == point2:
             return
         points = [point1, point2]
@@ -145,31 +164,40 @@ class DeliverybotControlNode(Node):
         self._action_door_client.wait_for_server()
         self._send_door_goal_future = self._action_door_client.send_goal_async(
             goal_msg, feedback_callback=self.feedback_callback)
-        self._send_door_goal_future.add_done_callback(self.goal_response_callback)
-        
+        self._send_door_goal_future.add_done_callback(
+            self.goal_response_callback)
+
     def send_steer_goal(self, steer_msg):
+        '''
+        steering_joints command controller to send joint_trajectory messages to control steering angle
+        :requires: joint_states topic and jointStates_callback - returns current door position information \n
+        :requires: cmd_vel topic - listens to cmd_vel.angular.z message for steering control
+        :returns: nothing - calls on action server
+        '''
         global vel_msg
         goal_msg = FollowJointTrajectory.Goal()
-        #Creating timing normalizer for duration of movement
+        # Creating timing normalizer for duration of movement
         norm_coeff = np.abs(steer_msg-self.steer_msg_prev) / 0.61085
         print(norm_coeff)
-        steer_time = norm_coeff * 0.5 #1.5 seconds to move from straight to max turn
+        steer_time = norm_coeff * 0.5  # 1.5 seconds to move from straight to max turn
         self.steer_msg_prev = steer_msg
 
-        #Creating joint controller messages
+        # Creating joint controller messages
         goal_msg = FollowJointTrajectory.Goal()
         points = []
         point1 = JointTrajectoryPoint()
         point1.positions = self.steer_pos_prev
-        
 
         point2 = JointTrajectoryPoint()
-        point2.time_from_start = Duration(seconds=steer_time, nanoseconds=0).to_msg()
+        point2.time_from_start = Duration(
+            seconds=steer_time, nanoseconds=0).to_msg()
 
         if steer_msg > 0:
-            self.steer_pos = [self.theta_in(steer_msg), self.theta_out(steer_msg)]
+            self.steer_pos = [self.theta_in(
+                steer_msg), self.theta_out(steer_msg)]
         if steer_msg < 0:
-            self.steer_pos = [self.theta_out(steer_msg), self.theta_in(steer_msg)]
+            self.steer_pos = [self.theta_out(
+                steer_msg), self.theta_in(steer_msg)]
         if steer_msg == 0:
             self.steer_pos = [0.0, 0.0]
 
@@ -178,14 +206,17 @@ class DeliverybotControlNode(Node):
 
         goal_msg.goal_time_tolerance = Duration(
             seconds=1, nanoseconds=0).to_msg()
-        goal_msg.trajectory.joint_names = ['front_right_steer_joint','front_left_steer_joint']
+        goal_msg.trajectory.joint_names = [
+            'front_right_steer_joint', 'front_left_steer_joint']
         goal_msg.trajectory.points = points
 
         self._action_steer_client.wait_for_server()
         self._send_steer_goal_future = self._action_steer_client.send_goal_async(
             goal_msg, feedback_callback=self.feedback_callback)
 
-        self._send_steer_goal_future.add_done_callback(self.goal_response_callback)
+        self._send_steer_goal_future.add_done_callback(
+            self.goal_response_callback)
+
     def goal_response_callback(self, future):
         goal_handle = future.result()
         if not goal_handle.accepted:
@@ -200,6 +231,7 @@ class DeliverybotControlNode(Node):
 
     def feedback_callback(self, feedback_msg):
         feedback = feedback_msg.feedback
+
 
 class JoySubscriberNode(Node):
 
@@ -220,7 +252,6 @@ class JoySubscriberNode(Node):
         vel_msg.linear.y = data.axes[0]*7.5
         vel_msg.angular.z = data.axes[3]*10
 
-
 class CmdVelSubscriberNode(Node):
 
     def __init__(self):
@@ -230,13 +261,11 @@ class CmdVelSubscriberNode(Node):
             'cmd_vel',
             self.cmdVel_callback,
             10)
+
     def cmdVel_callback(self, data):
         global vel_msg
         vel_msg = data
-        #super().send_door_goal()
-
-
-
+        # super().send_door_goal()
 
 def main(args=None):
     rclpy.init(args=None)
@@ -251,7 +280,7 @@ def main(args=None):
     executor_thread = threading.Thread(target=executor.spin, daemon=True)
     executor_thread.start()
 
-    rate=deliverybotControl.create_rate(2)
+    rate = deliverybotControl.create_rate(2)
     try:
         while rclpy.ok():
             rate.sleep()
@@ -260,6 +289,7 @@ def main(args=None):
 
     rclpy.shutdown()
     executor_thread.join()
+
 
 if __name__ == '__main__':
     main()
